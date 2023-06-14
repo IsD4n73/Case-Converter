bf0f97226d652842d9baec40dd0486bd1488e3d5
IsD4n73 <d..dante03@gmail.com>
2023-06-14 20:32:06 +0000

    init Sring Compare

---
+++ lib/view/strings_compare.dart
@@ -0,0 +1,23 @@
+import 'package:url_strategy/url_strategy.dart';
+
+class StringSimilarity extends StatelessWidget {
+  const StringSimilarity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+      TextEditingController stringController = TextEditingController();
+        TextEditingController string2Controller = TextEditingController();
+
+
+ 
+
+
+    return Scaffold(
+      bod: Column(
+        children: [
+
+        ], 
+      ),
+    );
+  }
+}
